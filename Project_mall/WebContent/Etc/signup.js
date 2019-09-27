ae4bdd4e7d1fa212e12ea70c54df40efef1ed154
estellechoi <estele.choi@gmail.com>
2019-09-27 15:14:06 +0900

    회원가입 페이지 JavaScript / 약관 전체동의 자동체크 함수, 이메일 호스트 자동입력 함수

--- Project_mall/WebContent/Etc/signup.js
+++ Project_mall/WebContent/Etc/signup.js
@@ -2,6 +2,17 @@
  * 
  */
 
+// 이메일 입력 선택
+function Host() {
+	var host = document.getElementById("email").value;
+	if(host == "직접입력") {
+		document.getElementById("email_host").value = "";
+	}
+	else {
+		document.getElementById("email_host").value = host;
+	}
+}
+
 // 전체동의 박스 체크/해제시, 모든 체크박스 체크/해제되는 스크립트
 
 function AgreeAll(master) {
@@ -20,7 +31,7 @@ function AgreeAll(master) {
 
 // 모든 체크박스가 체크되면 전체동의 박스도 자동 체크, 하나라도 체크되지 않았다면 전체동의 박스도 자동 해제
 
-function Check() {
+function Agree() {
 	var n = 0;
 	for(var i =0; i<4; i++) {
 		if(document.getElementsByClassName("agree")[i].checked) {
