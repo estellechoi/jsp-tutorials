29a5b27ab3362912069c18bd40fb9cac7a84140c
estellechoi <estele.choi@gmail.com>
2019-10-01 16:38:21 +0900

    사이드 네비게이션바 JavaScript / 하위카테고리 토글 / 대분류코드 get 전송

--- Project_mall/WebContent/Etc/nav.js
+++ Project_mall/WebContent/Etc/nav.js
@@ -2,7 +2,7 @@
  * 
  */
 
-function toggle(n) {
+function toggle(n, x) {
 	// 메뉴 숨기기
 	for (var i = 0; i < 4; i++) {
 		document.getElementsByClassName("sub")[i].style.display = "none";
@@ -10,4 +10,7 @@ function toggle(n) {
 
 	// 메뉴 보이기
 	document.getElementsByClassName("sub")[n].style.display = "block";
+
+	// 페이지 이동
+	location = "../Product/product_list.jsp?code=" + n + "&category="+x;
 }
