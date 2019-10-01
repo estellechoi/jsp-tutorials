0456b6c7f781ccc4724ff272dad5c9047256bc57
estellechoi <estele.choi@gmail.com>
2019-10-01 15:21:09 +0900

    테스트 Java 리소스 파일 / 인스턴트 메소드와 스태틱 메소드를 비교해보자 !

---
+++ Project_mall/src/mall/Test/Test.java
@@ -0,0 +1,18 @@
+package mall.Test;
+
+public class Test {
+
+	// 인스턴트 메소드
+	public int sum() {
+		int a = 100;
+		int b = 50;
+		return a + b;
+	}
+	
+	// 스태틱 메소드
+	public static int cal() {
+		int a =100;
+		int b = 50;
+		return a * b;
+	}
+}
