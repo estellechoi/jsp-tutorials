ced6d5ffa38dae04729dc195ff0316023192633f
estellechoi <estele.choi@gmail.com>
2019-10-02 17:38:26 +0900

    고객용 상품 상세 페이지 Javascript / AJAX, 레이어 보이기/숨기기

--- Project_mall/WebContent/Etc/product_content.js
+++ Project_mall/WebContent/Etc/product_content.js
@@ -2,12 +2,54 @@
  * 
  */
 
+function Close_cart() {
+	document.getElementById("layer_cart").display = "none";
+}
+
+function Move_cart() {
+	location = "../Member/cart.jsp";
+}
+
+function Close_wishlist() {
+	document.getElementById("layer_wishlist").display = "none";
+}
+
+function Move_wishlist() {
+	location = "../Member/wishlist.jsp";
+}
+
+// 장바구니/위시리스트 담기 AJAX
+
+var cart = new XMLHttpRequest();
+var wishlist = new XMLHttpRequest();
+
 function Cart(id) {
 	var size = document.getElementById("size").value;
 	var qty = document.getElementById("qty").value;
-	location = "cart_ok.jsp?id=" + id + "&size=" + size + "&qty=" + qty;
+	url = "cart_ok.jsp?id=" + id + "&size=" + size + "&qty=" + qty;
+	cart.open("get", url);
+	cart.send();
+}
+
+cart.onreadystatechange = function() {
+	if (cart.readyState == 4) {
+		if (cart.responseText.trim() == "ok") {
+			// JSP <html> 출력값이 "ok" 하나만 있어야 하며, 다른 값이 있을 경우 전달되지 않는다.
+			document.getElementById("layer_cart").display = "block";
+		}
+	}
 }
 
 function Wishlist(id) {
-	location = "wishlist_ok.jsp?id=" + id;
+	url = "wishlist_ok.jsp?id=" + id;
+	wishlist.open("get", url);
+	wishlist.send();
+}
+
+wishlist.onreadystatechange = function() {
+	if (wishlist.readyState == 4) {
+		if (wishlist.responseText.trim() == "ok") {
+			document.getElementById("layer_wishlist").display = "block";
+		}
+	}
 }
