32a836306ff6f3c20fa170c6c5ee1868ab222220
estellechoi <estele.choi@gmail.com>
2019-10-08 14:27:05 +0900

    배송지 주소록 페이지 / 새 창 열기 / 배송지 등록, 선택삭제, 적용 Javascript

---
+++ Project_mall/WebContent/Etc/product_buynow_address.js
@@ -0,0 +1,65 @@
+/**
+ * 
+ */
+// 배송지 등록
+function Add(email) {
+	location = "product_buynow_address_add.jsp?email="+email;
+}
+
+
+// 체크박스 자동 체크/해제
+function Check_all(master, n) {
+	if (master.checked) {
+		for (var i = 0; i < n; i++) {
+			document.getElementsByClassName("desti")[i].checked = true;
+		}
+	}
+	else {
+		for (var i = 0; i < n; i++) {
+			document.getElementsByClassName("desti")[i].checked = false;
+		}		
+	}
+}
+
+function Check_Master(n) {
+	for (var i = 0; i < n; i++) {
+		if (document.getElementsByClassName("desti")[i].checked) {
+			document.getElementById("checkbox_master").checked = true;
+		}
+		else {
+			document.getElementById("checkbox_master").checked = false;
+		}
+	}	
+}
+
+// 부모창에 값 전달하기
+function Opener(x, n, recipient, cell, zip, address1, address2) {
+	
+	var cell1 = cell.substring(0, 3);
+	var cell2 = cell.substring(3,7);
+	var cell3 = cell.substring(7, 11);
+
+	opener.form.r_username.value = recipient;
+	opener.form.r_cell1.value = cell1;
+	opener.form.r_cell2.value = cell2;
+	opener.form.r_cell3.value = cell3;
+	opener.form.r_zip.value = zip;
+	opener.form.r_address1.value = address1;
+	opener.form.r_address2.value = address2;
+	
+	// 모든 버튼 색상 디폴트로 변경
+	for (var i = 0; i < n; i ++) {
+		// document.querySelector(".apply")[i] 왜 안되지 ??? ;;;;
+		document.getElementsByClassName("apply")[i].style.background = "white";
+		document.getElementsByClassName("apply")[i].style.color = "#495164";
+	}
+	
+	// 적용된 주소록 버튼만 색상 다시 변경
+	x.style.background = "#495164";
+	x.style.color = "white";
+}
+
+// 창 닫기
+function Close() {
+	close();
+}
