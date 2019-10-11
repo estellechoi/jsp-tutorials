42e789e58f26556d28517247ac136b82abdd76f8
estellechoi <estele.choi@gmail.com>
2019-10-11 17:40:49 +0900

    JSP 페이지에서 사용할 커맨드 메소드 / DAO 객체의 메소드를 실행하는 메소드

--- Project_mall/src/mall/Command/Qna_write.java
+++ Project_mall/src/mall/Command/Qna_write.java
@@ -3,21 +3,21 @@ package mall.Command;
 import java.sql.Connection;
 import java.sql.SQLException;
 import mall.Jdbc.Connect;
-import mall.Dao.Qna_insert;
-import mall.Dto.Qna;
+import mall.Dao.Qna_dao;
+import mall.Dto.Qna_dto;
 
 public class Qna_write {
 
 	// 커맨드 실행 메소드 (변수를 담은 Dto 객체)
-	public void write(Qna dto) throws SQLException {
+	public void write(Qna_dto dto) throws SQLException {
 		
 		// DB 연결
 		Connection conn = Connect.connection_static();
 		
 		// 쿼리실행 메소드를 가진 Dao 객체 생성
-		Qna_insert dao = new Qna_insert();
+		Qna_dao dao = new Qna_dao();
 		
-		// Dao 클래스의 쿼리실행 메소드
+		// Dao의 메소드 실행
 		dao.insert(conn, dto);
 	}
 }
