1fd477986c5315550a2f8255ef12c8588dfaa975
estellechoi <estele.choi@gmail.com>
2019-10-18 14:18:20 +0900

    address 테이블 레코드로 들어갈 값들을 저장하는 변수

---
+++ Project_mall/src/mall/Dto/Address.java
@@ -0,0 +1,77 @@
+package mall.Dto;
+
+public class Address {
+	
+	private int id;
+	private String email;
+	private String destination;
+	private String recipient;
+	private String zip;
+	private String address1;
+	private String address2;
+	private String cell;
+	private int isdefault;
+	private String writeday;
+	
+	
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	public String getDestination() {
+		return destination;
+	}
+	public void setDestination(String destination) {
+		this.destination = destination;
+	}
+	public String getRecipient() {
+		return recipient;
+	}
+	public void setRecipient(String recipient) {
+		this.recipient = recipient;
+	}
+	public String getZip() {
+		return zip;
+	}
+	public void setZip(String zip) {
+		this.zip = zip;
+	}
+	public String getAddress1() {
+		return address1;
+	}
+	public void setAddress1(String address1) {
+		this.address1 = address1;
+	}
+	public String getAddress2() {
+		return address2;
+	}
+	public void setAddress2(String address2) {
+		this.address2 = address2;
+	}
+	public String getCell() {
+		return cell;
+	}
+	public void setCell(String cell) {
+		this.cell = cell;
+	}
+	public int getIsdefault() {
+		return isdefault;
+	}
+	public void setIsdefault(int isdefault) {
+		this.isdefault = isdefault;
+	}
+	public String getWriteday() {
+		return writeday;
+	}
+	public void setWriteday(String writeday) {
+		this.writeday = writeday;
+	}
+}
