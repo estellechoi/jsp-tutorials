882c7a09325c477b290d64ffeb7b62c88102e119
estellechoi <estele.choi@gmail.com>
2019-10-24 09:38:08 +0900

    update 쿼리문을 실행하는 Edit(conn, dto) 메소드 / dto.getUsername() 값 사용

--- Project_mall/src/mall/Dao/Account_dao.java
+++ Project_mall/src/mall/Dao/Account_dao.java
@@ -12,8 +12,30 @@ public class Account_dao {
 	// 전달된 값을 가진 DTO 객체를 매개변수로 사용하여 값을 불러오자 (getter 사용)
 	public void Edit(Connection conn, Member dto) throws SQLException {
 		
+		// 새로 입력한 주소가 기존 address 테이블에 있는 주소인지 검사
+		String id_address = null;
+		String sql = "select*from address where email='" + dto.getEmail() + "' and address1='" + dto.getAddress1() + "'";
+		Statement stmt = conn.createStatement();
+		ResultSet rs = stmt.executeQuery(sql);
+		
+		// 이미 있다면 id_address 값을 가져온다
+		if (rs.next()) {
+			id_address = rs.getString("id_address");
+		}
+		// 없다면 address 테이블에 새 주소를 추가하고, 그 id 값을 가져와서 id_address 값으로 삼는다.
+		else {
+			sql = "insert into address(email, recipient, zip, address1, address2, cell, writeday, destination)";
+			sql = " values('"+dto.getEmail()+"', '"+dto.getUsername()+"', '"+dto.getZip()+"', '"+dto.getAddress1()+"', '"+dto.getAddress2()+"', '"+dto.getCell()+"', now(), '집')";
+			stmt.executeUpdate(sql);
+			
+			// 방금 address 테이블에 추가한 주소의 id값
+			sql = "select max(id) as id from address";
+			rs = stmt.executeQuery(sql);
+			rs.next();
+			id_address = rs.getString("id");
+		}
 		// member 테이블 수정 쿼리
-		String sql = "update member set username=?, email=?, zip=?, address1=?, address2=?, cell=?, sex=?, birth=?, agree_SMS=?, agree_email=? where id=?";
+		sql = "update member set username=?, email=?, zip=?, address1=?, address2=?, cell=?, sex=?, birth=?, agree_SMS=?, agree_email=?, id_address=? where id=?";
 		PreparedStatement pstmt = conn.prepareStatement(sql);
 		pstmt.setString (1, dto.getUsername());
 		pstmt.setString(2, dto.getEmail());
@@ -25,8 +47,11 @@ public class Account_dao {
 		pstmt.setString(8, dto.getBirth());
 		pstmt.setString(9, dto.getAgree_SMS());
 		pstmt.setString(10, dto.getAgree_email());
-		pstmt.setInt(11, dto.getId());
+		pstmt.setString(11, id_address);
+		pstmt.setInt(12, dto.getId());
 		pstmt.execute();
+		// 종료
+		stmt.close();
 		pstmt.close();
 		conn.close();
 	}
